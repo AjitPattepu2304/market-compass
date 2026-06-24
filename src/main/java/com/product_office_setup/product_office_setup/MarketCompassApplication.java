fc252d5acc80fb0acfe0f8063757445dfa2c234e
Ajit Pattepu - vn552f7 <Ajit.Pattepu@walmart.com>
2026-06-24 16:28:12 -0500

    refactor: rename app class to MarketCompassApplication, remove unused Cassandra dependency
    🌀 Magic applied with Wibey JetBrains Plugin 🪄

---
+++ src/main/java/com/product_office_setup/product_office_setup/MarketCompassApplication.java
@@ -0,0 +1,12 @@
+package com.product_office_setup.product_office_setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketCompassApplication.class, args);
+	}
+}
--- src/test/java/com/product_office_setup/product_office_setup/MarketCompassApplicationTests.java
+++ src/test/java/com/product_office_setup/product_office_setup/MarketCompassApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductOfficeSetupApplicationTests {
+class MarketCompassApplicationTests {
 
 	@Test
 	void contextLoads() {
